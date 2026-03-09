822503c43856264baf17cbcb6e782fb7be13c40f
jm1021 <jmort1021@gmail.com>
2026-03-09 11:55:15 -0700

    port change

--- main.py
+++ main.py
@@ -325,6 +325,6 @@ app.cli.add_command(custom_cli)
 # this runs the flask application on the development server
 if __name__ == "__main__":
     host = "0.0.0.0"
-    port = app.config['FLASK_PORT']
+    port = app.config['8367']
     print(f"** Server running: http://localhost:{port}")  # Pretty link
     app.run(debug=True, host=host, port=port, use_reloader=False)
